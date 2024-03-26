3dad2c7aed7d63f7567e9c987d4153f7da7dca1d
zbuse <5518208+zbuse@users.noreply.github.com>
2024-03-26 11:32:05 +0800

    Update StripeCredit.php

--- app/Payments/StripeCredit.php
+++ app/Payments/StripeCredit.php
@@ -119,7 +119,7 @@ class StripeCredit {
 
     private function exchange($from, $to)
     {
-        $result = file_get_contents('https://api.exchangerate.host/latest?symbols=' . $to . '&base=' . $from);
+        $result = file_get_contents('https://api.vatcomply.com/rates?base='. $from);
         $result = json_decode($result, true);
         return $result['rates'][$to];
     }
